7c5c9e43e7dba32543824bd61e495fbee46ac272
James Nodws <Nodws@users.noreply.github.com>
2018-01-22 18:25:07 -0700

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 	<title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
 	<meta charset="utf-8">
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
