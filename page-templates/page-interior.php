6999baa82d8e787e4b88d409f9dcec821e658c75
James Nodws <Nodws@users.noreply.github.com>
2018-12-13 13:59:17 -0700

    Update page-interior.php

--- page-templates/page-interior.php
+++ page-templates/page-interior.php
@@ -6,8 +6,9 @@
  */
 ?>
 <?php get_header();
+$cover = (has_post_thumbnail()) ? "background-image:url(".get_the_post_thumbnail_url(get_the_ID(), 'full').")" : '';
 ?>
-<div class="cover" style="background-image:url(<? the_post_thumbnail_url( 'full' );  ?>)">
+<div class="cover" style="<?=$cover?>">
 	<div class="container">
 		<h2><?php the_title()?></h2>	
 	</div>
