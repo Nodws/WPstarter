d8854a57cc9d411fa2a128ff252aefccc32cfe5b
James Nodws <Nodws@users.noreply.github.com>
2019-04-17 10:37:10 -0600

    footer class

--- footer.php
+++ footer.php
@@ -1,23 +1,18 @@
-<footer class="site-footer">
-  <hr/>
-
-	<div class="container">
+<footer id="footer" class="site-footer">
+ <div class="container">
+	<div class="row footer-cols">
+         <?php dynamic_sidebar('footer-widget-area'); ?>
+        </div>
+	<div class="row footer-full">
+         <?php dynamic_sidebar('footer-full'); ?>
+        </div>
 	<div class="row">
-    <?php dynamic_sidebar('footer-widget-area'); ?>
-  </div>
-	<div class="row">
-    <?php dynamic_sidebar('footer-full'); ?>
-  </div>
-
-  <div class="row">
-    <div class="col">
-      <p class="text-center">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?> </a>| <? bloginfo('description'); ?></p>
-    </div>
-  </div>
-</div>
+	   <div class="col">
+	     <p class="text-center">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?> </a> | <? bloginfo('description'); ?></p>
+	   </div>
+	</div>
+ </div>
 </footer>
-
-
 <?php wp_footer(); ?>
 </body>
 </html>
