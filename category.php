5bffe0456ad5992612c990da0ca62c18e7b94cbd
James Nodws <Nodws@users.noreply.github.com>
2019-05-02 17:57:35 -0600

    nav

--- category.php
+++ category.php
@@ -8,6 +8,9 @@
         <h1>Category: <?php echo single_cat_title(); ?></h1>
         <hr>
         <?php get_template_part('loops/content', get_post_format()); ?>
+          <nav class="pagination">
+    <? wpst_pagination(); ?>
+    </nav>
       </div><!-- /#content -->
     </div>
     
