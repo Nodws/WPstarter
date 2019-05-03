cff54572f2e26bb17cef7abee5d982f6dc1de848
James Nodws <Nodws@users.noreply.github.com>
2019-05-03 11:58:40 -0600

    Update single.php

--- single.php
+++ single.php
@@ -3,13 +3,13 @@
 <div class="container">
   <div class="row">
     
-    <div class="col-sm-8">
+    <div class="col-sm-9">
       <div id="content" role="main">
         <?php get_template_part('loops/content', get_post_type()); ?>
       </div><!-- /#content -->
     </div>
     
-    <div class="col-sm-4" id="sidebar" role="navigation">
+    <div class="col-sm-3" id="sidebar" role="navigation">
        <?php get_sidebar(); ?>
     </div>
     
