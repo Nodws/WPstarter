dc4c37737877e758da99f3af8891847ec340a29e
James Nodws <Nodws@users.noreply.github.com>
2019-05-03 11:56:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
 <div class="container">
+    <header><h1> <?php echo ucfirst($wp_query->query[pagename]); ?></h1></header>
   <div class="row">
     
-    <div class="col-sm-8">
+    <div class="col-sm-9">
       <div id="content" role="main">
           <?php get_template_part('loops/content', get_post_format()); ?>
       </div><!-- /#content -->  
@@ -12,7 +13,7 @@
     </nav>
     </div>
     
-    <div class="col-sm-4" id="sidebar" role="navigation">
+    <div class="col-sm-3" id="sidebar" role="navigation">
        <?php get_sidebar(); ?>
     </div>
     
